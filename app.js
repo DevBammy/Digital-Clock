c81a7ce4093edbb27a5b8b4b47af17b8583c0bd3
devBammy <74296296+DevBammy@users.noreply.github.com>
2022-07-31 07:47:59 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+function clock() {
+  const hour = document.querySelector(".hour");
+  const minutes = document.querySelector(".minutes");
+  const seconds = document.querySelector(".seconds");
+
+  const h = new Date().getHours();
+  const m = new Date().getMinutes();
+  const s = new Date().getSeconds();
+
+  hour.innerHTML = h;
+  minutes.innerHTML = m;
+  seconds.innerHTML = s;
+}
+
+setInterval(clock, 1000);
+
+// hour.innerHTML = h;
